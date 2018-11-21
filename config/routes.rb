b3b2928862841f419e3ad263d3cc429cc7ad45a9
Angus Watson <angus.watson16@gmail.com>
2018-11-21 15:53:20 +0000

    finished lender user journey

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,4 @@ Rails.application.routes.draw do
   get 'pages/applicant', to: 'pages#applicant', as: 'applicant'
   get 'pages/lender', to: 'pages#lender', as: 'lender'
 
-  get '/edit_applicant', to: 'users#edit_applicant', as: 'edit_applicant'
-  get '/edit_lender', to: 'users#edit_lender', as: 'edit_lender'
-  patch '/update_lender', to: 'users#update_lender', as: 'update_lender'
-  patch '/update_applicant', to: 'users#update_applicant', as: 'update_applicant'
-
 end
