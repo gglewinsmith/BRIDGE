880e4830edddeeb6af0d8685034089b1270bf486
Angus Watson <angus.watson16@gmail.com>
2018-11-21 10:41:31 +0000

    changes made tuesday evening regarding params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:homepage, :applicant, :lender]
 
   def homepage
+
   end
 
   def applicant_dashboard
