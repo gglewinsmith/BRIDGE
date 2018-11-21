e34618e4cf61ff92c06c16f8e694a5151c92dd23
Angus Watson <angus.watson16@gmail.com>
2018-11-21 12:32:52 +0000

    form submits applicant as true in background

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:homepage, :applicant, :lender]
 
   def homepage
-
   end
 
   def applicant_dashboard
