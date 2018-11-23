7de325d7502233d918cc9db6d1df307ec1889514
George Lewin-Smith <gls1@hotmail.co.uk>
2018-11-23 16:41:57 +0000

    don

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { host: ENV["www.bridgeloans.website"] || "localhost:3000" }
+    { host: "www.bridgeloans.website" }
   end
 
   private
