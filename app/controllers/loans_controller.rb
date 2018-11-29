90874b4cbd883adf092564acfd221345a366f7d2
Angus Watson <angus.watson16@gmail.com>
2018-11-29 14:34:34 +0000

    fixed bug issues with amount raised etc

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -9,10 +9,9 @@ class LoansController < ApplicationController
       fundraising_event_id: @fundraising_event.id
     )
     @fundraising_event.amount_raised += @loan.amount
+    @loan.loan_length = @fundraising_event.loan_length
     @loan.amount_owed += (((@loan.amount * (1.003333 ** @fundraising_event.loan_length)) / 100).round) * 100
     @fundraising_event.amount_due += @loan.amount_owed
-    @fundraising_event.amount_due = @loan.amount_owed
-    @loan.loan_length = @fundraising_event.loan_length
     if @fundraising_event.amount_raised >= @fundraising_event.price
       @fundraising_event.status = 'fulfilled'
     else
